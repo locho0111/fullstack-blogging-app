69ad9f01bd164f75e94f150dfe7b51d53708976e
rick <iossoares1101@gmail.com>
2024-10-04 18:12:32 -0500

    Added terraform EKS cluster and deployment files

---
+++ terraform/applications/EKS-cluster/README.md
@@ -0,0 +1,1 @@
+aws eks update-kubeconfig --region us-east1 --name my-eks-cluster
---
+++ terraform/applications/EKS-cluster/c3-eks.tf
@@ -0,0 +1,47 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.name
+  cluster_version = "1.28"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t2.micro", "t2.small", "t2.medium"]
+
+    attach_cluster_primary_security_group = true
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    eks_workers = {
+      desired_capacity = 1
+      min_capacity     = 1
+      max_capacity     = 3
+      instance_type    = "t2.medium"
+    }
+  }
+
+  tags = local.tags
+}
+
